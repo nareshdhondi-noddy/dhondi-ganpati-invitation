da43e2ff727a5a8594c9c66a93315c32e3d79549
nareshdhondi-noddy <naresh.dhondi@gmail.com>
2026-09-09 13:37:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 
   /* --- Configuration -------------------------------------- */
   var CONFIG = {
-    mapsUrl: 'https://maps.app.goo.gl/cRY1QJZiN5nMUKr87',
+    mapsUrl: 'https://maps.app.goo.gl/xZmBJUmcRuLjp1X96',
     showWhatsApp: true,
     showPetals: true,
     shareMessage: 'Ganpati Bappa Morya! You and your family are invited for darshan ' +
